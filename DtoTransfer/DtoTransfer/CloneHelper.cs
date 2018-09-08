bbe8a87e17ff90e2d5e00ef90393a83c4abd00ad
liaohang <953917332@qq.com>
2018-09-08 11:18:51 +0800

    123123123

---
+++ DtoTransfer/DtoTransfer/CloneHelper.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace DtoTransfer
+{
+    public static class CloneHelper<TIn, TOut>
+    {
+        #region 基于表达式树  高性能
+
+
+        //Todo 转换
+
+        private static readonly Func<TIn, TOut> cache = GetFunc();
+        private static Func<TIn, TOut> GetFunc()
+        {
+            ParameterExpression parameterExpression = Expression.Parameter(typeof(TIn), "p");
+            List<MemberBinding> memberBindingList = new List<MemberBinding>();
+
+            foreach (var item in typeof(TOut).GetProperties())
+            {
+                if (!item.CanWrite || typeof(TIn).GetProperty(item.Name) == null)
+                    continue;
+
+                MemberExpression property = Expression.Property(parameterExpression, typeof(TIn).GetProperty(item.Name));
+                MemberBinding memberBinding = Expression.Bind(item, property);
+                memberBindingList.Add(memberBinding);
+            }
+
+            MemberInitExpression memberInitExpression = Expression.MemberInit(Expression.New(typeof(TOut)), memberBindingList.ToArray());
+            Expression<Func<TIn, TOut>> lambda = Expression.Lambda<Func<TIn, TOut>>(memberInitExpression, new ParameterExpression[] { parameterExpression });
+
+            return lambda.Compile();
+        }
+
+        public static TOut Trans(TIn tIn)
+        {
+            return cache(tIn);
+        }
+        //Todo 更新
+        public static TOut Update(TIn tin, TOut tout)
+        {
+            return updatecache(tin, tout);
+        }
+        private static readonly Func<TIn, TOut, TOut> updatecache = UpdateFunc();
+
+        private static Func<TIn, TOut, TOut> UpdateFunc()
+        {
+            ParameterExpression parameterExpression = Expression.Parameter(typeof(TIn), "p");
+            ParameterExpression outerExpression = Expression.Parameter(typeof(TOut), "out");
+            List<MemberBinding> memberBindingList = new List<MemberBinding>();
+
+            Parallel.ForEach(typeof(TOut).GetProperties(), item => {
+                if ( item.CanWrite)
+                {
+                    if (typeof(TIn).GetProperty(item.Name) == null)
+                    {
+                        MemberExpression property = Expression.Property(outerExpression, typeof(TOut).GetProperty(item.Name));
+                        MemberBinding memberBinding = Expression.Bind(item, property);
+                        memberBindingList.Add(memberBinding);
+                    }
+                    else
+                    {
+                        MemberExpression property = Expression.Property(parameterExpression, typeof(TIn).GetProperty(item.Name));
+                        MemberBinding memberBinding = Expression.Bind(item, property);
+                        memberBindingList.Add(memberBinding);
+                    }
+                }
+                
+           
+            });
+         
+
+            MemberInitExpression memberInitExpression = Expression.MemberInit(Expression.New(typeof(TOut)), memberBindingList.ToArray());
+            Expression<Func<TIn, TOut, TOut>> lambda = Expression.Lambda<Func<TIn, TOut, TOut>>(memberInitExpression, new ParameterExpression[] { parameterExpression, outerExpression });
+
+            return lambda.Compile();
+        }
+        #endregion
+
+
+        #region  基于反射 性能低
+
+        public static TOut TransDto(TIn tin)
+        {
+
+            var tout = Activator.CreateInstance<TOut>();
+            var Pros = typeof(TIn).GetProperties().Join(typeof(TOut).GetProperties(),
+                inner =>
+                    new
+                    {
+                        Name = inner.Name,
+                        PType = inner.PropertyType
+                    },
+                outter => new { Name = outter.Name, PType = outter.PropertyType }
+                , (inner, outter) =>
+                   inner
+            );
+            foreach (var item in Pros)
+            {
+                var value = item.GetValue(tin);
+                if (!item.PropertyType.IsGenericType)
+                {
+                    //Todo 如果当前类型是泛型类型，则为 true；否则为 false。
+                    item.SetValue(tout, value == null ? null : Convert.ChangeType(value, item.PropertyType), null);
+                }
+                else
+                {
+                    Type genericTypeDefinition = item.PropertyType.GetGenericTypeDefinition();
+                    if (genericTypeDefinition == typeof(Nullable<>))
+                    {
+                        //如果是值类型
+                        item.SetValue(tout, value == null ? null : Convert.ChangeType(value, Nullable.GetUnderlyingType(item.PropertyType)));
+                    }
+                    else
+                    {
+                        //不考虑非空泛型  比如说IList 这些
+                    }
+                }
+            }
+
+            return tout;
+
+        }
+
+        public static TOut UpdateDto(TIn tin, TOut tout)
+        {
+            Type typeIn = typeof(TIn);
+            var Pros = typeof(TIn).GetProperties().Join(typeof(TOut).GetProperties(),
+                inner =>
+                    new
+                    {
+                        Name = inner.Name,
+                        PType = inner.PropertyType
+                    },
+                outter => new { Name = outter.Name, PType = outter.PropertyType }
+
+                , (inner, outter) =>
+
+                        outter
+                    );
+
+
+            foreach (var item in Pros)
+            {
+                var value = typeIn.GetProperty(item.Name).GetValue(tin);
+                if (!item.PropertyType.IsGenericType)
+                {
+                    //Todo 如果当前类型是泛型类型，则为 true；否则为 false。
+                    item.SetValue(tout, value == null ? null : Convert.ChangeType(value, item.PropertyType), null);
+                }
+                else
+                {
+                    Type genericTypeDefinition = item.PropertyType.GetGenericTypeDefinition();
+                    if (genericTypeDefinition == typeof(Nullable<>))
+                    {
+                        //如果是可空泛型
+                        item.SetValue(tout, value == null ? null : Convert.ChangeType(value, Nullable.GetUnderlyingType(item.PropertyType)));
+                    }
+                    else
+                    {
+                        //不考虑非空泛型  比如说IList 这些
+                    }
+                }
+            }
+            return tout;
+        }
+        #endregion
+
+    }
+    /// <summary>
+    /// AutoMapper扩展帮助类
+    /// </summary>
+    public static class AutoMapperHelper 
+    {
+          
+       
+
+
+
+
+        private static Dictionary<Type, Type> typesDic = new Dictionary<Type, Type>();
+        /// <summary>
+        ///  类型映射
+        /// </summary>
+        public static TDes UpdateTo<T,TDes> (T obj, TDes des)
+        {
+            if (obj == null) return des; 
+            return Mapper.Map<T,TDes>(obj,des);
+        }
+
+    }
+}
---
+++ DtoTransfer/DtoTransfer/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace DtoTransfer
+{
+    class Base : AutoMapper.Profile
+    {
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<StuA, Stu>();
+
+            });
+            var st = new Stu()
+            {
+                Id = 1,
+                Name = "aasdf",
+                Age = 4,
+                Money = 100
+            };
+            var strA = new StuA()
+            {
+                Age = 555555,
+                Name = "DDDDDDDDDDDDDD",
+                Money = 12
+            };
+
+            var time = 10000000;
+            var start = DateTime.Now;
+            for (int i = 0; i < time; i++)
+            {
+                var tem = CloneHelper<StuA, Stu>.Update(strA, st);
+            }
+            Console.WriteLine("表达式树:" + (DateTime.Now - start).Milliseconds);
+
+            var startB = DateTime.Now;
+            for (int i = 0; i < time; i++)
+            {
+                var tem = AutoMapper.Mapper.Map<StuA, Stu>(strA, st);
+            }
+
+            Console.WriteLine("AutoMapper:" + (DateTime.Now - startB).Milliseconds);
+
+
+            //start = DateTime.Now;
+            //for (int i = 0; i < time; i++)
+            //{
+            //    var tem = CloneHelper<StuA, Stu>.UpdateDto(strA, st);
+            //}
+
+            //Console.WriteLine("基于反射" + (DateTime.Now - start).Milliseconds);
+
+            var startA = DateTime.Now;
+            for (int i = 0; i < time; i++)
+            {
+                var tem = AutoMapperHelper.UpdateTo(strA, st);
+            }
+
+            Console.WriteLine("AutoMapper Helper" + (DateTime.Now - startA).Milliseconds);
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class Stu
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Money { get; set; }
+
+    }
+
+    public class StuA
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Money { get; set; }
+
+    }
+}
